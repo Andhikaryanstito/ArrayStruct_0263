36dd383a5aa45a7b5f1e93ae03cc14eca51e6b20
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-04-15 11:06:10 +0700

    Menambahkan struct mahasiswa(nim,nama,alamat)

---
+++ struct.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+};
+
+int main(){
+    Mahasiswa mhs;
+
+    mhs.nim = "20220140102";
+    mhs.nama = "Abra Yudhistira";
+    mhs.alamat = "Kasihan";
+
+}
